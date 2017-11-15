c435d9a2097d6bf050f20ebb06ce6e43d35e8c4b
pingpong <codingpingjun@gmail.com>
2017-11-15 00:56:50 -0500

    finish read all the codes

--- train/train_pap.py
+++ train/train_pap.py
@@ -4,23 +4,15 @@ import os, sys, pdb
 import argparse
 import torch
 
-HOME_PATH = os.path.expanduser('~')
 FILE_PATH = os.path.abspath(__file__)
 PRJ_PATH = os.path.dirname(os.path.dirname(FILE_PATH))
 sys.path.append(PRJ_PATH)
 
-try:
-    from .papSmear.proj_utils.local_utils import mkdirs
-    from .papSmear.datasets.papsmear import papSmearData as Dataset
-    from .papSmear.cfgs.config_pap import cfg
-    from .papSmear.darknet import Darknet19
-    from .papSmear.train_yolo import train_eng
-except:
-    from papSmear.proj_utils.local_utils import mkdirs
-    from papSmear.datasets.papsmear import papSmearData as Dataset
-    from papSmear.cfgs.config_pap import cfg
-    from papSmear.darknet import Darknet19
-    from papSmear.train_yolo import train_eng
+from papSmear.proj_utils.local_utils import mkdirs
+from papSmear.datasets.papsmear import papSmearData as Dataset
+from papSmear.cfgs.config_pap import cfg
+from papSmear.darknet import Darknet19
+from papSmear.train_yolo import train_eng
 
 
 def set_args():
@@ -28,7 +20,7 @@ def set_args():
     parser = argparse.ArgumentParser(description = 'Pap Smear Bounding Box Detection')
 
     parser.add_argument('--device_id',  type=int, default=0, help='which device')
-    parser.add_argument('--batch_size', type=int, default=6, help='batch size.')
+    parser.add_argument('--batch_size', type=int, default=8, help='batch size.')
     parser.add_argument('--img_size',   default=[256, 320, 352], help='output image size')
 
     parser.add_argument('--maxepoch',        type=int,   default=20000,    help='number of epochs to train (default: 10)')
@@ -38,7 +30,7 @@ def set_args():
     parser.add_argument('--momentum',        type=float, default=0.9,      help='SGD momentum (default: 0.5)')
     parser.add_argument('--weight_decay',    type=float, default=0,        help='weight decay for training')
 
-    parser.add_argument('--reuse_weights',   action='store_true', default =True, help='continue from last checkout point')
+    parser.add_argument('--reuse_weights',   action='store_true', default=False, help='continue from last checkout point')
     parser.add_argument('--load_from_epoch', type=int, default= 800, help='load from epoch')
 
     parser.add_argument('--display_freq',    type=int, default= 200, help='plot the results every {} batches')
@@ -51,19 +43,16 @@ def set_args():
 
 if  __name__ == '__main__':
     args = set_args()
-
-    model_root = os.path.join(PRJ_PATH, 'Model')
-    mkdirs([model_root])
-    # Set data_root
-    data_root = os.path.join(HOME_PATH, 'DataSet/PapSmear/CellBoundingBox/TrainDataset')
-
+    model_root = os.path.join(PRJ_PATH, 'models')
+    data_root = os.path.join(PRJ_PATH, 'data/training')
 
     # Dataloader setting
-    dataloader = Dataset(data_root, args.batch_size, img_shape = (256, 256))  #img_size is changable
+    dataloader = Dataset(data_root, args.batch_size, img_shape = (256, 256))
 
     # Set Darknet
     net = Darknet19(cfg)
-    # print(net)
+
+    pdb.set_trace()
 
     # CUDA Settings
     cuda_avail = torch.cuda.is_available()
